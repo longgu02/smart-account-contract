b78138592a124839b8f08d0a9346fb08660d5dca
longgu02 <phamlong121120022@gmail.com>
2024-04-03 16:59:43 +0700

    feat: account exec plugin

--- contracts/Account.sol
+++ contracts/Account.sol
@@ -22,6 +22,7 @@ import {AccountLoupe} from "./plugins/AccountLoupe.sol";
 import {FunctionReferenceLib} from "./helpers/FunctionReferenceLib.sol";
 import {FunctionReference, IPluginManager} from "./interfaces/IPluginManager.sol";
 import {IPlugin, PluginManifest} from "./interfaces/IPlugin.sol";
+import {IPluginExecutor} from "./interfaces/IPluginExecutor.sol";
 // Factory
 import {SingleOwnerPlugin} from "./plugins/base/owner/SingleOwnerPlugin.sol";
 
@@ -31,16 +32,17 @@ contract Account is
     IAccount,
     ModuleManager,
     SmartAccountErrors,
+    IPluginExecutor,
     AccountStorageInitializable,
     PluginManagerInternals
 {
-    uint public count;
-    address public owner;
-    mapping(address => bool) public signable;
-    IEntryPoint private immutable _ENTRY_POINT;
-
     IEntryPoint private immutable ENTRY_POINT;
     address private immutable SELF;
+
+    struct PostExecToRun {
+        bytes preExecHookReturnData;
+        FunctionReference postExecHook;
+    }
 
     constructor(
         address _owner,
@@ -55,6 +57,27 @@ contract Account is
         ENTRY_POINT = IEntryPoint(_entryPointAddress);
         SELF = address(this);
     }
+
+    error UnrecognizedFunction(bytes4 selector);
+    error ExecFromPluginNotPermitted(address plugin, bytes4 selector);
+    error ExecFromPluginExternalNotPermitted(
+        address plugin,
+        address target,
+        uint256 value,
+        bytes data
+    );
+    error NativeTokenSpendingNotPermitted(address plugin);
+    error AlwaysDenyRule();
+    error PostExecHookReverted(
+        address plugin,
+        uint8 functionId,
+        bytes revertReason
+    );
+    error PreExecHookReverted(
+        address plugin,
+        uint8 functionId,
+        bytes revertReason
+    );
 
     fallback() external payable {}
 
@@ -117,6 +140,110 @@ contract Account is
         _installPlugin(plugin, manifestHash, pluginInstallData, dependencies);
     }
 
+    /// @inheritdoc IPluginExecutor
+    function executeFromPlugin(
+        bytes calldata data
+    ) external payable override returns (bytes memory) {
+        bytes4 selector = bytes4(data[:4]);
+        address callingPlugin = msg.sender;
+
+        bytes24 execFromPluginKey = getPermittedCallKey(
+            callingPlugin,
+            selector
+        );
+
+        AccountStorage storage _storage = getAccountStorage();
+
+        if (!_storage.callPermitted[execFromPluginKey]) {
+            revert ExecFromPluginNotPermitted(callingPlugin, selector);
+        }
+
+        address execFunctionPlugin = _storage.selectorData[selector].plugin;
+
+        if (execFunctionPlugin == address(0)) {
+            revert UnrecognizedFunction(selector);
+        }
+
+        // PostExecToRun[] memory postExecHooks = _doPreExecHooks(selector, data);
+
+        (bool success, bytes memory returnData) = execFunctionPlugin.call(data);
+
+        if (!success) {
+            assembly ("memory-safe") {
+                revert(add(returnData, 32), mload(returnData))
+            }
+        }
+
+        // _doCachedPostExecHooks(postExecHooks);
+
+        return returnData;
+    }
+
+    /// @inheritdoc IPluginExecutor
+    function executeFromPluginExternal(
+        address target,
+        uint256 value,
+        bytes calldata data
+    ) external payable returns (bytes memory) {
+        bytes4 selector = bytes4(data);
+        AccountStorage storage _storage = getAccountStorage();
+
+        // Make sure plugin is allowed to spend native token.
+        if (
+            value > 0 &&
+            value > msg.value &&
+            !_storage.pluginData[msg.sender].canSpendNativeToken
+        ) {
+            revert NativeTokenSpendingNotPermitted(msg.sender);
+        }
+
+        // Check the caller plugin's permission to make this call
+
+        // Check the target contract permission.
+        // This first checks that the intended target is permitted at all. If it is, then it checks if any selector
+        // is permitted. If any selector is permitted, then it skips the selector-level permission check.
+        // If only a subset of selectors are permitted, then it also checks the selector-level permission.
+        // By checking in the order of [address specified with any selector allowed], [any address allowed],
+        // [address specified and selector specified], along with the extra bool `permittedCall`, we can
+        // reduce the number of `sload`s in the worst-case from 3 down to 2.
+        bool targetContractPermittedCall = _storage
+        .permittedExternalCalls[IPlugin(msg.sender)][target].addressPermitted &&
+            (_storage
+            .permittedExternalCalls[IPlugin(msg.sender)][target]
+                .anySelectorPermitted ||
+                _storage
+                .permittedExternalCalls[IPlugin(msg.sender)][target]
+                    .permittedSelectors[selector]);
+
+        // If the target contract is not permitted, check if the caller plugin is permitted to make any external
+        // calls.
+        if (
+            !(targetContractPermittedCall ||
+                _storage.pluginData[msg.sender].anyExternalExecPermitted)
+        ) {
+            revert ExecFromPluginExternalNotPermitted(
+                msg.sender,
+                target,
+                value,
+                data
+            );
+        }
+
+        // Run any pre exec hooks for this selector
+        // PostExecToRun[] memory postExecHooks = _doPreExecHooks(
+        //     IPluginExecutor.executeFromPluginExternal.selector,
+        //     msg.data
+        // );
+
+        // Perform the external call
+        bytes memory returnData = _call(target, value, data);
+
+        // Run any post exec hooks for this selector
+        // _doCachedPostExecHooks(postExecHooks);
+
+        return returnData;
+    }
+
     /// @inheritdoc IPluginManager
     function uninstallPlugin(
         address plugin,
@@ -167,7 +294,12 @@ contract Account is
         );
     }
 
-    function _call(address target, uint256 value, bytes memory data) internal {
+    function _call(
+        address target,
+        uint256 value,
+        bytes memory data
+    ) internal returns (bytes memory) {
+        bytes memory returndata;
         assembly {
             let success := call(
                 gas(),
@@ -178,12 +310,14 @@ contract Account is
                 0,
                 0
             )
-            let ptr := mload(0x40)
-            returndatacopy(ptr, 0, returndatasize())
+            let returndata_size := returndatasize()
+            returndata := mload(0x40)
+            returndatacopy(returndata, 0, returndata_size)
             if iszero(success) {
-                revert(ptr, returndatasize())
+                revert(returndata, returndata_size)
             }
         }
+        return returndata;
     }
 
     function execute_ncC(
@@ -233,6 +367,131 @@ contract Account is
             }
         }
     }
+
+    // function _doPreExecHooks(
+    //     bytes4 selector,
+    //     bytes calldata data
+    // ) internal returns (PostExecToRun[] memory postHooksToRun) {
+    //     HookGroup storage hooks = getAccountStorage()
+    //         .selectorData[selector]
+    //         .executionHooks;
+    //     uint256 preExecHooksLength = hooks.preHooks.length();
+    //     uint256 postOnlyHooksLength = hooks.postOnlyHooks.length();
+    //     uint256 maxPostExecHooksLength = postOnlyHooksLength;
+
+    //     // There can only be as many associated post hooks to run as there are pre hooks.
+    //     for (uint256 i = 0; i < preExecHooksLength; ) {
+    //         (, uint256 count) = hooks.preHooks.at(i);
+    //         unchecked {
+    //             maxPostExecHooksLength += (count + 1);
+    //             ++i;
+    //         }
+    //     }
+
+    //     // Overallocate on length - not all of this may get filled up. We set the correct length later.
+    //     postHooksToRun = new PostExecToRun[](maxPostExecHooksLength);
+    //     uint256 actualPostHooksToRunLength;
+
+    //     // Copy post-only hooks to the array.
+    //     for (uint256 i = 0; i < postOnlyHooksLength; ) {
+    //         (bytes32 key, ) = hooks.postOnlyHooks.at(i);
+    //         postHooksToRun[actualPostHooksToRunLength]
+    //             .postExecHook = _toFunctionReference(key);
+    //         unchecked {
+    //             ++actualPostHooksToRunLength;
+    //             ++i;
+    //         }
+    //     }
+
+    //     // Then run the pre hooks and copy the associated post hooks (along with their pre hook's return data) to
+    //     // the array.
+    //     for (uint256 i = 0; i < preExecHooksLength; ) {
+    //         (bytes32 key, ) = hooks.preHooks.at(i);
+    //         FunctionReference preExecHook = _toFunctionReference(key);
+
+    //         if (preExecHook.isEmptyOrMagicValue()) {
+    //             // The function reference must be PRE_HOOK_ALWAYS_DENY in this case, because zero and any other
+    //             // magic value is unassignable here.
+    //             revert AlwaysDenyRule();
+    //         }
+
+    //         bytes memory preExecHookReturnData = _runPreExecHook(
+    //             preExecHook,
+    //             data
+    //         );
+
+    //         uint256 associatedPostExecHooksLength = hooks
+    //             .associatedPostHooks[preExecHook]
+    //             .length();
+    //         if (associatedPostExecHooksLength > 0) {
+    //             for (uint256 j = 0; j < associatedPostExecHooksLength; ) {
+    //                 (key, ) = hooks.associatedPostHooks[preExecHook].at(j);
+    //                 postHooksToRun[actualPostHooksToRunLength]
+    //                     .postExecHook = _toFunctionReference(key);
+    //                 postHooksToRun[actualPostHooksToRunLength]
+    //                     .preExecHookReturnData = preExecHookReturnData;
+
+    //                 unchecked {
+    //                     ++actualPostHooksToRunLength;
+    //                     ++j;
+    //                 }
+    //             }
+    //         }
+
+    //         unchecked {
+    //             ++i;
+    //         }
+    //     }
+
+    //     // Trim the post hook array to the actual length, since we may have overallocated.
+    //     assembly ("memory-safe") {
+    //         mstore(postHooksToRun, actualPostHooksToRunLength)
+    //     }
+    // }
+
+    // function _doCachedPostExecHooks(
+    //     PostExecToRun[] memory postHooksToRun
+    // ) internal {
+    //     uint256 postHooksToRunLength = postHooksToRun.length;
+    //     for (uint256 i = postHooksToRunLength; i > 0; ) {
+    //         unchecked {
+    //             --i;
+    //         }
+
+    //         PostExecToRun memory postHookToRun = postHooksToRun[i];
+    //         (address plugin, uint8 functionId) = postHookToRun
+    //             .postExecHook
+    //             .unpack();
+    //         // solhint-disable-next-line no-empty-blocks
+    //         try
+    //             IPlugin(plugin).postExecutionHook(
+    //                 functionId,
+    //                 postHookToRun.preExecHookReturnData
+    //             )
+    //         {} catch (bytes memory revertReason) {
+    //             revert PostExecHookReverted(plugin, functionId, revertReason);
+    //         }
+    //     }
+    // }
+
+    // function _runPreExecHook(
+    //     FunctionReference preExecHook,
+    //     bytes calldata data
+    // ) internal returns (bytes memory preExecHookReturnData) {
+    //     (address plugin, uint8 functionId) = preExecHook.unpack();
+    //     try
+    //         IPlugin(plugin).preExecutionHook(
+    //             functionId,
+    //             msg.sender,
+    //             msg.value,
+    //             data
+    //         )
+    //     returns (bytes memory returnData) {
+    //         preExecHookReturnData = returnData;
+    //     } catch (bytes memory revertReason) {
+    //         revert PreExecHookReverted(plugin, functionId, revertReason);
+    //     }
+    // }
 }
 
 contract AccountFactory {
